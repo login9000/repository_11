7d25c6078568baa0376b86337321147e94a74e11
login9000 <pmax462@yahoo.com>
2025-11-21 22:55:09 +0300

    update

--- app/Http/Controllers/ControllerDownloadOrderDetails.php
+++ app/Http/Controllers/ControllerDownloadOrderDetails.php
@@ -439,7 +439,7 @@ class ControllerDownloadOrderDetails extends Common{
 		// 	chmod($this->document_root . $link, 0664);
 		// }
 		
-		$link .= '?'.time();
+		$link .= '?_='.time();
 		
 		parent::prepare_response(['response' => ['link' => $link]]);
 		
