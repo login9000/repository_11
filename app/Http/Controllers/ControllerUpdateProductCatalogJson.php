20448f3d5f6d757f8e86c15893442c110d7213ff
login9000 <pmax462@yahoo.com>
2025-12-23 20:53:35 +0300

    update

--- app/Http/Controllers/ControllerUpdateProductCatalogJson.php
+++ app/Http/Controllers/ControllerUpdateProductCatalogJson.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
+use App\Helpers\Common;
 
-class ControllerUpdateProductCatalogJson {
+class ControllerUpdateProductCatalogJson extends Common {
 	
 	public function __invoke(){
 		
