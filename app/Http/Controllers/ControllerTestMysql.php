ab5e2b7477899f29cde828f4e4dbd681da5ed2a7
login9000 <pmax462@yahoo.com>
2025-12-23 19:45:49 +0300

    update

--- app/Http/Controllers/ControllerTestMysql.php
+++ app/Http/Controllers/ControllerTestMysql.php
@@ -12,22 +12,22 @@ class ControllerTestMysql extends Common{
 	
 	public function __invoke(){
 		
-			//return '<br>exit!!!!!!!!!!!!';
+			return '<br>exit!!!!!!!!!!!!';
 		
 		
-			$data = '';
-			$result = DB::select('SELECT `data` FROM `product_catalog` WHERE `id` = 1 AND `data` != \'\' LIMIT 1');
+			// $data = '';
+			// $result = DB::select('SELECT `data` FROM `product_catalog` WHERE `id` = 1 AND `data` != \'\' LIMIT 1');
 			
-			foreach ($result as $row) {
-				$data =$row->data;
-			}
+			// foreach ($result as $row) {
+				// $data =$row->data;
+			// }
 			
-			$f = fopen($this->document_root.'/../public/product_catalog.json','a+');
-			ftruncate($f, 0);
-			stream_set_write_buffer($f, 0); 
-			fwrite($f, $data);
-			fflush($f);
-			fclose($f);
+			// $f = fopen($this->document_root.'/../public/product_catalog.json','a+');
+			// ftruncate($f, 0);
+			// stream_set_write_buffer($f, 0); 
+			// fwrite($f, $data);
+			// fflush($f);
+			// fclose($f);
 		
 		 exit('Done.');
 		 
