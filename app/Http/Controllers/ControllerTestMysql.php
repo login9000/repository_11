d4f210b3add30e3ddc9b8451ab1ee942a75c3807
login9000 <pmax462@yahoo.com>
2026-01-14 14:24:52 +0300

    update

--- app/Http/Controllers/ControllerTestMysql.php
+++ app/Http/Controllers/ControllerTestMysql.php
@@ -12,22 +12,31 @@ class ControllerTestMysql extends Common{
 	
 	public function __invoke(){
 		
-			return '<br>exit!!!!!!!!!!!!';
-		
-		
-			// $data = '';
-			// $result = DB::select('SELECT `data` FROM `product_catalog` WHERE `id` = 1 AND `data` != \'\' LIMIT 1');
+			//return '<br>exit!!!!!!!!!!!!';
 			
-			// foreach ($result as $row) {
-				// $data =$row->data;
-			// }
+			// json_encode() - из массива в строку
+			// json_decode() - из строки в массив
 			
-			// $f = fopen($this->document_root.'/../public/product_catalog.json','a+');
-			// ftruncate($f, 0);
-			// stream_set_write_buffer($f, 0); 
-			// fwrite($f, $data);
-			// fflush($f);
-			// fclose($f);
+			//echo is_array(json_encode([], JSON_UNESCAPED_UNICODE)) ? 'массив' : 'не массив' . '<br>';
+			
+			//exit;
+			
+			
+			$data = '';
+			$result = DB::select('SELECT `data` FROM `product_catalog` WHERE `id` = 1 AND `data` != \'\' LIMIT 1');
+			
+			foreach ($result as $row) {
+			 $data = $row->data;
+			}
+
+			$product_catalog = parent::transformation_product_catalog(json_decode($data, true));
+		
+			$f = fopen($this->document_root.'/../public/product_catalog.json','a+');
+			ftruncate($f, 0);
+			stream_set_write_buffer($f, 0); 
+			fwrite($f, json_encode($product_catalog, JSON_UNESCAPED_UNICODE));
+			fflush($f);
+			fclose($f);
 		
 		 exit('Done.');
 		 
